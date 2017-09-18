a9f54b1997bdc21f2bfe3d9e8d4290ca69950dc9
Joseph Psychas <iopsychas@gmail.com>
2017-09-18 13:29:16 +0300

    Add page footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer () {
+  return (
+    <div className='footer'>
+      Made with <i className='fa fa-heart' /> by <a href='http://iopsycas.me'>iopsychas</a>.
+      This project is on <a href='#'><i className='fa fa-github' /></a>
+    </div>
+  );
+}
+
+export default Footer;
